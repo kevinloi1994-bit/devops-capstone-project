584b0601c04636c975024cfb64735c03edae672c
kevinloi1994-bit <kevin.loi1994@gmail.com>
2026-03-13 02:44:48 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,13 +2,7 @@ from flask import Flask
 from flask_talisman import Talisman
 from flask_cors import CORS
 
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    # Enable security headers
-    Talisman(app)
-
-    # Enable CORS
-    CORS(app)
-
-    return app
+Talisman(app)
+CORS(app)
